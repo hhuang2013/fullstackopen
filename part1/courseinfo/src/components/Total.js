7ce0f2e889487ba24aa0192bbacd1212b8bca2a8
Hill <herohest@gmail.com>
2020-07-03 14:40:39 +0800

    use array reduce

--- part1/courseinfo/src/components/Total.js
+++ part1/courseinfo/src/components/Total.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Total = ({ parts }) => (
     <>
-        <p>Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}</p>
+        <p>Number of exercises {parts.reduce((s, p) => s + p.exercises, 0)}</p>
     </>
 )
 
